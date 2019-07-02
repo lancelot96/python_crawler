53912187203e4a939c3b08fc07f6ba1aa91d32bf
Zeyu Shi <lancelot96@protonmail.ch>
2019-07-02 17:13:38 +0800

    get_links link_crawler 函数

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import re
+import time
+import queue
 import requests
+from urllib import parse
+
+
+def get_links(html):
+    # <a href="?discussion_start=10#comment-section">2</a>
+    web_regex = re.compile(r"<a +href=[\'\"](.*?)[\'\"]")
+    return web_regex.findall(html)
 
 
 def download(url, num_retries=3, user_agent="wswp", proxies=None):
@@ -20,7 +30,28 @@ def download(url, num_retries=3, user_agent="wswp", proxies=None):
             break
 
 
-url = "https://movie.douban.com/subject/26727273/episode/1/"
+def link_cwraler(start_url, link_regex, delay=5):
+    seen = set()
+    crawler_queue = queue.Queue()
+    crawler_queue.put(start_url)
 
-html = download(url)
-print(html)
+    while not crawler_queue.empty():
+        url = crawler_queue.get()
+
+        time.sleep(delay)
+        html = download(url)
+        # TODO
+
+        for link in get_links(html):
+            if link and re.match(link_regex, link):
+                abs_link = parse.urljoin(url, link)
+                if abs_link not in seen:
+                    crawler_queue.put(abs_link)
+                    seen.add(abs_link)
+
+
+url = "https://movie.douban.com/subject/26727273/episode/1/"
+link_regex = r"\?discussion_start="
+
+
+link_cwraler(url, link_regex)
